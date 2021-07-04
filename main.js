0bb3bf00cb39613b32f916ecf1d8d29e2fe85a68
jayditya123 <81817676+jayditya123@users.noreply.github.com>
2021-07-04 13:55:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log ("Jay");
+var firstname="Jay ";
+console.log (firstname)
+var lastname= "Chadha"
+console.log (lastname)
+var fullname= firstname.concat(lastname)
+console.log (fullname)
+var lengthname= fullname.length
+console.log (lengthname)
+var n1= 10
+console.log (n1)
+var n2= 9567465
+console.log (n2)
+console.log (n1+n2)
+console.log (n1*n2)
